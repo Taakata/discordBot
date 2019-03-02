ad5c774f1e3669ab12febbb436587129da63a281
Arthur C <48140962+Taanaka@users.noreply.github.com>
2019-03-01 21:20:11 -0300

    📔 I don't have ideas

---
+++ src/utils/vars.js
@@ -0,0 +1,5 @@
+let { Attachment, Client, Collection, RichEmbed } = require("discord.js")
+let { readdirSync, readFileSync } = require("fs")
+let { join } = require("path")
+
+module.exports = { Attachment, Client, Collection, RichEmbed, readdirSync, readFileSync, join }
